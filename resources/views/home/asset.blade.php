82c48ce877d20483096819fc97e07bc530650e23
Oluyosola <afolabioluyosola@gmail.com>
2021-07-25 21:46:12 +0100

    frontend fixed

--- resources/views/home/asset.blade.php
+++ resources/views/home/asset.blade.php
@@ -13,30 +13,30 @@
     <link href="{{ asset ('assets/lib/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
 
     <!-- Libraries CSS Files -->
-        <link href="{{ asset ('assets/lib/font-awesome/css/font-awesome.min.css')}}" rel="stylesheet">
-        <link href="{{ asset ('assets/lib/animate/animate.min.css')}}" rel="stylesheet">
-        <link href="{{ asset ('assets/lib/ionicons/css/ionicons.min.css')}}" rel="stylesheet">
-        <link href="{{ asset ('assets/lib/owl.carousel/assets/owl.carousel.min.css')}}" rel="stylesheet">
-        <link href="{{asset ('assets/lib/icofont/icofont.min.css')}}" rel="stylesheet">
-        <link href="{{ asset ('assets/lib/jquery/magnific-popup.css')}}" rel="stylesheet">
-        <link href="{{ asset ('assets/lib/aos/aos.css')}}" rel="stylesheet">
-        <link href="{{ asset ('assets/lib/venobox/venobox.css')}}" rel="stylesheet">
-        <link href="{{ asset ('assets/lib/icofont/icofont.min.css')}}" rel="stylesheet">
+        <link href="{{asset('assets/lib/font-awesome/css/font-awesome.min.css')}}" rel="stylesheet">
+        <link href="{{asset('assets/lib/animate/animate.min.css')}}" rel="stylesheet">
+        <link href="{{asset('assets/lib/ionicons/css/ionicons.min.css')}}" rel="stylesheet">
+        <link href="{{asset('assets/lib/owl.carousel/assets/owl.carousel.min.css')}}" rel="stylesheet">
+        <link href="{{asset('assets/lib/icofont/icofont.min.css')}}" rel="stylesheet">
+        <link href="{{asset('assets/lib/jquery/magnific-popup.css')}}" rel="stylesheet">
+        <link href="{{asset('assets/lib/aos/aos.css')}}" rel="stylesheet">
+        <link href="{{asset('assets/lib/venobox/venobox.css')}}" rel="stylesheet">
+        <link href="{{asset('assets/lib/icofont/icofont.min.css')}}" rel="stylesheet">
         <script src="https://www.google.com/recaptcha/api.js" async defer></script>
         <link href="{{asset('assets/css/frontend_style_blue.css')}}" rel="stylesheet">
         
         <!-- JavaScript Libraries -->
         
-        <script src="{{ asset('assets/lib/jquery/jquery.min.js')}}"></script>
-        <script src="{{ asset('assets/lib/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
-        <script src="{{ asset('assets/lib/jquery.easing/jquery.easing.min.js')}}"></script>
-        <script src="{{ asset('assets/lib/php-email-form/validate.js')}}"></script>
-        <script src="{{ asset('assets/lib/waypoints/jquery.waypoints.min.js')}}"></script>
-        <script src="{{ asset('assets/lib/counterup/counterup.min.js')}}"></script>
-        <script src="{{ asset('assets/lib/isotope-layout/isotope.pkgd.min.js')}}"></script>
-        <script src="{{ asset('assets/lib/venobox/venobox.min.js')}}"></script>
-        <script src="{{ asset('assets/lib/owl.carousel/owl.carousel.min.js')}}"></script>
-        <script src="{{ asset('assets/lib/aos/aos.js')}}"></script>
+        <script src="{{asset('assets/lib/jquery/jquery.min.js')}}"></script>
+        <script src="{{asset('assets/lib/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+        <script src="{{asset('assets/lib/jquery.easing/jquery.easing.min.js')}}"></script>
+        <script src="{{asset('assets/lib/php-email-form/validate.js')}}"></script>
+        <script src="{{asset('assets/lib/waypoints/jquery.waypoints.min.js')}}"></script>
+        <script src="{{asset('assets/lib/counterup/counterup.min.js')}}"></script>
+        <script src="{{asset('assets/lib/isotope-layout/isotope.pkgd.min.js')}}"></script>
+        <script src="{{asset('assets/lib/venobox/venobox.min.js')}}"></script>
+        <script src="{{asset('assets/lib/owl.carousel/owl.carousel.min.js')}}"></script>
+        <script src="{{asset('assets/lib/aos/aos.js')}}"></script>
 
         <!-- assetslate Main Javascript File -->
         <script src="{{ asset('assets/js/main.js')}}"></script>
